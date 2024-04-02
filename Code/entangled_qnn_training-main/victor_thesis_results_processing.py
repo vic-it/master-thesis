523471a5ec837a34cfc416e3e9dfc1137a116405
Victor Ülger <victor.uelger@web.de>
2024-04-02 12:15:08 +0200

    improve metric visulaizations

--- Code/entangled_qnn_training-main/victor_thesis_results_processing.py
+++ Code/entangled_qnn_training-main/victor_thesis_results_processing.py
@@ -2,6 +2,7 @@ from matplotlib import pyplot as plt
 from matplotlib.ticker import MaxNLocator
 import numpy as np
 
+from victor_thesis_plots import plot_results_metric
 from victor_thesis_utils import calc_average_of_std
 class Result:     
     def __init__(self, idx):
@@ -17,6 +18,9 @@ class Result:
         self.SC_std_abs_list=[]
 class Combined_Result:
     def __init__(self):
+        self.config_indices = []
+        self.number_configs = 0
+        self.number_runs = 0
         self.TV_avg= 0
         self.TV_std= 0
         self.FD_avg= 0
@@ -27,8 +31,8 @@ class Combined_Result:
         self.SC_neg_avg= 0
         self.SC_avg= 0
         self.SC_std= 0
-        self.SC_avg_abs= 0
-        self.SC_std_abs= 0
+        self.SC_abs_avg= 0
+        self.SC_abs_std= 0
 
 def read_results(idx, max_run_id):
     results = []
@@ -95,7 +99,10 @@ def combine_results(result_list):
     SC_avg_abs_list=[]
     SC_std_abs_list=[]
     #concatenate all lists
-    for result in result_list:        
+    for result in result_list: 
+        combined_results.config_indices.append(result.idx) 
+        combined_results.number_configs += 1
+        combined_results.number_runs += 25      
         TV_list+=result.TV_list
         FD_list+=result.FD_list
         IGSD_list+=result.IGSD_list
@@ -118,28 +125,38 @@ def combine_results(result_list):
     combined_results.SC_pos_avg=np.mean(SC_pos_list)
     combined_results.SC_neg_avg=np.mean(SC_neg_list)
     combined_results.SC_avg=np.mean(SC_avg_list)
-    combined_results.SC_avg_abs=np.mean(SC_avg_abs_list)
+    combined_results.SC_abs_avg=np.mean(SC_avg_abs_list)
     # SC std stuff
     combined_results.SC_std= calc_average_of_std(SC_std_list)
-    combined_results.SC_std_abs=calc_average_of_std(SC_std_abs_list)
+    combined_results.SC_abs_std=calc_average_of_std(SC_std_abs_list)
     return combined_results
 
-def std_plot(mean_list, average_list, y_label, x_label):
-    x_axis = range(1,len(mean_list)+1)
-    plt.figure().gca().xaxis.set_major_locator(MaxNLocator(integer=True))
-    plt.errorbar(x_axis, mean_list, average_list, linestyle='None', marker='o', capsize=5)
-    plt.ylabel(y_label)
-    plt.xlabel(x_label)
-    plt.title("Mean and Standard Deviation")
-    plt.show()
-
-def process_results(idx):
+def visualize_metrics(combined_results_list, x_label, title, sample_labels = range(1, 5)):    
+    attr_list = ["TV", "IGSD","FD", "SC", "SC_abs"]
+    combined_mean_list = []
+    combined_std_list =[]
+    for attr_name in attr_list:
+        combined_results_mean = []
+        combined_results_std = []
+        for res in combined_results_list:
+            combined_results_mean.append(getattr(res,f"{attr_name}_avg"))
+            combined_results_std.append(getattr(res,f"{attr_name}_std"))
+        combined_mean_list.append(combined_results_mean)
+        combined_std_list.append(combined_results_std)
+    pos_list = []
+    neg_list = []
+    for res in combined_results_list:
+        pos_list.append(res.SC_pos_avg)
+        neg_list.append(res.SC_neg_avg)
+    plot_results_metric(combined_mean_list, combined_std_list, pos_list, neg_list, attr_list, x_label, title, sample_labels)
+    
+def get_results(idx):
     # condensed configs from 1600total runs -> 320 configXunitary -> into 64 configurations, consiting of lists of results
     configs = generate_config()
     #print(configs)
     results = read_results(idx, len(configs))
-    #5 consecutive confs are the same config over 5 different unitaries -> combine them first by averaging
+    return results, configs
 
 
 run_id = "10_6_3_26_14_44_31"
-process_results(run_id)
+get_results(run_id)
